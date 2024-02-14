543d62b3d309c5347a4dab7e75e6fa5c982e9188
Sushant Yadav <yadav_sushant@ymail.com>
2024-02-14 22:13:10 +0530

    Login, logout completed

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -26,7 +26,7 @@ class UserController extends Controller
         $this->ip_address = $req->ip();
         try{
             $validator = Validator::make($req->all(), [
-                'user_name' => 'required|string|exists:users,contact_no',
+                'user_name' => 'required|string|exists:users,contact_no,status,1',
                 'password' => 'required|string',
             ]);
             if ($validator->fails()) {
@@ -82,8 +82,101 @@ class UserController extends Controller
 
         Helper::updateUserLog(["user_id"=>$this->user_id, "user_name"=>$req->user_name??"", "parent"=>"authentication", "parent_id"=> "", "action"=>$this->action, "response"=>$response, "browser"=>$this->browser, "ip_address"=>$this->ip_address]);
 
-        // unset($response["error"]["details"]);
+        unset($response["error"]["details"]);
         return response()->json($response,$this->statusCode);
-
     }
+    function logout(Request $req){
+        $this->action = 'logout';
+        $this->browser = $req->header('User-Agent');
+        $this->ip_address = $req->ip();
+        try{
+            $user = Auth::user();
+            $this->user_id = $user_id??0;
+            $token = $user->token();
+            $token->revoke();
+            $this->response['description'] = 'User logged out successfully.';
+        }
+        catch(ErrorException | Error $e){
+            $this->status = false;
+            $this->statusCode = 500;
+            $this->error['code'] = 'UC0005';
+            $this->error['title'] = 'INTERNAL_ERROR';
+            $this->error['description'] = 'Syntax Error.';
+            $this->error['details'] = ['message' => $e->getMessage()];
+        }
+        $response = ["status"=>$this->status,"statusCode"=>$this->statusCode,"error"=>$this->error,"response"=>$this->response];
+
+        Helper::updateUserLog(["user_id"=>$this->user_id, "parent"=>"authentication", "parent_id"=> "", "action"=>$this->action, "browser"=>$this->browser, "ip_address"=>$this->ip_address, "request"=>$req->all(), "response"=>$response]);
+
+        unset($response["error"]["details"]);
+
+        return response()->json($response,$this->statusCode);
+    }
+
+    function getUserTable(Request $req){
+        try{
+            $validator = Validator::make($req->all(), [
+                'start'    => 'nullable|numeric',
+                'length'     => 'required|numeric'
+            ]);
+            if ($validator->fails()) {
+                $this->status = false;
+                $this->statusCode = 403;
+                $this->error['code'] = 'UC0006';
+                $this->error['title'] = 'VALIDATION_ERROR';
+                $this->error['description'] = 'These fields are required.';
+                $this->error['fields'] = $validator->errors();
+            }
+            else{
+
+                $input = array(
+                    'searchValues'  => ($req->search)?$req->search['value']:'',
+                    'status'        => $req->conditions['status']??1,
+                    'columns'       => ($req->columns)?$req->columns:[],
+                    'order'         => ($req->order)?$req->order:[],
+                    'offset'        => $req->start??0,
+                    'limit'         => $req->length??0
+                );
+                $data = Tms_user::getRecords($input);
+
+                $input = array(
+                    'searchValues'  => ($req->search)?$req->search['value']:'',
+                    'status'        => $req->conditions['status']??1,
+                    'offset'        => $req->start??0,
+                    'limit'         => $req->length??0,
+                );
+                $recordsFiltered = Tms_user::getRecordsFiltered($input);
+
+                $input = array(
+                    'status'        => $req->conditions['status']??1,
+                );
+                $recordsTotal = Tms_user::getRecordsTotal($input);
+
+                $this->response['recordsTotal'] = $recordsTotal;
+                $this->response['recordsFiltered'] = $recordsFiltered;
+                $this->response['data'] = $data;
+            }
+        }
+        catch(ErrorException | Error $e){
+            $this->status = false;
+            $this->statusCode = 500;
+            $this->error['code'] = 'UC0007';
+            $this->error['title'] = 'INTERNAL_ERROR';
+            $this->error['description'] = 'Syntax Error.';
+            $this->error['details'] = ['message' => $e->getMessage()];
+        }
+        catch(RelationNotFoundException | QueryException $e){
+            $this->status = false;
+            $this->statusCode = 502;
+            $this->error['code'] = 'UC0008';
+            $this->error['title'] = 'DB_ERROR';
+            $this->error['description'] = 'SQL Syntax Error.';
+            $this->error['details'] = ['message' => $e->getMessage()];
+        }
+        $response = ["status"=>$this->status,"statusCode"=>$this->statusCode,"error"=>$this->error,"response"=>$this->response];
+
+        unset($response["error"]["details"]);
+        return response()->json($response,$this->statusCode);
+    }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,20 @@ use App\Http\Controllers\UserController;
 */
 
 Route::post('login',[UserController::class,'login']);
+
+Route::group(['middleware' => 'auth:api'], function(){
+    Route::post('logout',[UserController::class,'logout']);
+
+    /*
+    | --------------------------------------------------------------------------
+    | User APIs
+    | --------------------------------------------------------------------------
+    */
+    Route::post('user-table',[UserController::class,'getUserTable'])->name('accounting.users.table');
+    Route::get('user-list',[UserController::class,'getUserList']);
+    Route::post('user/{add}',[UserController::class,'addUpdateUser'])->name('accounting.users.add');  
+    Route::post('user/{update}',[UserController::class,'addUpdateUser'])->name('accounting.users.update');  
+    Route::post('user/delete',[UserController::class,'deleteUser'])->name('accounting.users.delete');  
+    Route::post('user/get',[UserController::class,'getUser'])->name('accounting.users.get');  
+    Route::post('user/get_details',[UserController::class,'getUserDetails'])->name('accounting.users.get_details'); 
+});
